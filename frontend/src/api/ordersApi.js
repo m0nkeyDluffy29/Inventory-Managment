d703027f74e897753450f4562d21893f7fdd3b6b
DakshSharma <dakshsharma292003@gmail.com>
2026-07-12 17:41:42 +0530

    api issue fix

--- frontend/src/api/ordersApi.js
+++ frontend/src/api/ordersApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: (import.meta.env.VITE_API_URL || "") + "/api",
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("inv_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
